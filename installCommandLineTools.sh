10181aca7dd0fd261b64842f4c9c5730fcbea596
Delgado, David <>
2021-11-07 20:34:17 +0100

    improve script

--- installCommandLineTools.sh
+++ installCommandLineTools.sh
@@ -4,12 +4,9 @@ echo "👁 Installing Xcode Command Line tools.."
 sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
 sudo xcodebuild -runFirstLaunch
 sudo xcodebuild -license
-open -a Simulator
-
 sudo gem install cocoapods
 
 echo "👁 Installing node.."
-brew update
 brew install node
 echo "👁 Installing Firebase CLI.."
 npm install -g firebase-tools
